ba2a50c02beec6ff7bc13ed00474811737ab473c
Веревин <d.verevin@vsemayki.ru>
2021-01-29 18:26:44 +0700

    Change slider component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,53 @@
 import Header from '@components/Header/Header';
 import Slider from '@components/Slider/Slider';
+import { isMobile } from 'react-device-detect';
 
 const IndexPage = () => {
+    if (!process.browser) return null;
     return (
         <>
             <Header />
-            <Slider />
+            <Slider
+                watchOverflow={true}
+                loop={true}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                navigation
+                pagination={{ clickable: true }}
+            >
+                {[
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                    {
+                        big: '//via.placeholder.com/1900x300',
+                        small: '//via.placeholder.com/375x300',
+                    },
+                ].map((el) => (
+                    <img
+                        src={isMobile ? el.small : el.big}
+                        className="full-size"
+                    />
+                ))}
+            </Slider>
         </>
     );
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,51 +1,17 @@
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade } from 'swiper';
-import { isMobile } from 'react-device-detect';
 
 SwiperCore.use([EffectFade]);
 import styles from './Slider.module.scss';
 
-const Slider = () => {
-    const big = '//via.placeholder.com/1900x300';
-    const small = '//via.placeholder.com/375x300';
-
-    if (!process.browser) return null;
-
+const Slider: React.FunctionComponent<Swiper> = (props) => {
     return (
         <div className={styles['slider']}>
-            <Swiper
-                watchOverflow={true}
-                loop={true}
-                slidesPerView={1}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                navigation
-                pagination={{ clickable: true }}
-            >
-                <SwiperSlide>
-                    <img
-                        src={isMobile ? small : big}
-                        className={styles['slider__img']}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        src={isMobile ? small : big}
-                        className={styles['slider__img']}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        src={isMobile ? small : big}
-                        className={styles['slider__img']}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        src={isMobile ? small : big}
-                        className={styles['slider__img']}
-                    />
-                </SwiperSlide>
+            <Swiper {...props}>
+                {React.Children.map(props.children, (child) => (
+                    <SwiperSlide>{child}</SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
