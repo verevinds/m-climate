23ced4a650efcf9ce7a4d5514a0743dd9ea66ea4
Веревин Денис <d.verevin@vsemayki.ru>
2021-01-29 15:22:03 +0700

    Add adaptive

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,10 +1,14 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade } from 'swiper';
+import { isMobile } from 'react-device-detect';
 
 SwiperCore.use([EffectFade]);
 import styles from './Slider.module.scss';
 
 const Slider = () => {
+    const big = '//via.placeholder.com/1900x300';
+    const small = '//via.placeholder.com/375x300';
+
     return (
         <div className={styles['slider']}>
             <Swiper
@@ -13,19 +17,20 @@ const Slider = () => {
                 slidesPerView={1}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
+                navigation
                 pagination={{ clickable: true }}
             >
                 <SwiperSlide>
-                    <img src="//via.placeholder.com/1900x300" />
+                    <img src={isMobile ? small : big} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="//via.placeholder.com/1900x300" />
+                    <img src={isMobile ? small : big} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="//via.placeholder.com/1900x300" />
+                    <img src={isMobile ? small : big} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="//via.placeholder.com/1900x300" />
+                    <img src={isMobile ? small : big} />
                 </SwiperSlide>
             </Swiper>
         </div>
