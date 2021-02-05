f4664a31bf4efd0f7f2a3c4940680f90d05761b7
Веревин Денис <d.verevin@vsemayki.ru>
2021-02-05 07:08:15 +0700

    Add get initial props

--- pages/admin/brand.tsx
+++ pages/admin/brand.tsx
@@ -1,25 +1,34 @@
+import type { AppContext } from 'next/app';
 import LayoutAdmin from '@components/Layout/LayoutAdmin';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ActiveLink from '@src/lib/ActiveLink';
 import styles from 'src/scss/pages/brand.module.scss';
+import { NextPage } from 'next';
 
-const Brand = () => {
+const Brand: NextPage<Pick<AppContext['ctx'], 'pathname' | 'query'>> = ({
+    pathname,
+}) => {
     return (
         <LayoutAdmin>
-            <h1>Настройка брендов</h1>
-            <ActiveLink
-                href={'/admin/brand?type=new'}
-                activeClassName={styles['-active']}
-                exec={false}
-            >
-                <a>
-                    <span>Создать</span>
-                    <FontAwesomeIcon icon={faPlusCircle} />
-                </a>
-            </ActiveLink>
+            <div>
+                <h1>Настройка брендов</h1>
+                <ActiveLink
+                    href={`${pathname}?type=new`}
+                    activeClassName={styles['-active']}
+                    exec={false}
+                >
+                    <a>
+                        <span>Создать</span>
+                        <FontAwesomeIcon icon={faPlusCircle} />
+                    </a>
+                </ActiveLink>
+            </div>
         </LayoutAdmin>
     );
+};
+Brand.getInitialProps = async ({ pathname, query }) => {
+    return { pathname, query };
 };
 
 export default Brand;
