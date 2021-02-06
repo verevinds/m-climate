4c52f4b3294e2175ab0d0c1f5cd6775da56751cf
Веревин <d.verevin@vsemayki.ru>
2021-02-06 13:20:24 +0700

    Change setting

--- src/components/AdminCreate/AdminCreate.tsx
+++ src/components/AdminCreate/AdminCreate.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 export interface IAdminCreate {}
 
 const AdminCreate: React.FC<IAdminCreate> = () => {
-  return <></>;
+  return <>a</>;
 };
 
 export default React.memo(AdminCreate);
