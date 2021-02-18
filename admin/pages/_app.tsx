cd076b5f6cae7ac0aaef9ab619366ada7cc25474
Веревин Денис <d.verevin@vsemayki.ru>
2021-02-18 11:23:05 +0700

    Delete never used styles

--- admin/pages/_app.tsx
+++ admin/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@src/scss/styles.scss';
 import '@verevinds/ui-kit/dist/styles.global.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 import withReduxStore from '@lib/with-redux-store';
 import { StoreWithPersist } from '@redux/index';
