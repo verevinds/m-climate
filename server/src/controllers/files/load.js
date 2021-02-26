6587e751b51deda1920d4c8f82be400e5109d6b0
Веревин Денис <d.verevin@vsemayki.ru>
2021-02-26 08:17:36 +0700

    Add handle errors

--- server/src/controllers/files/load.js
+++ server/src/controllers/files/load.js
@@ -3,40 +3,51 @@ var path = require('path');
 var fs = require('fs');
 
 exports.load = (req, res) => {
-  if (req.files) {
-    const { file } = req.files;
-    const filename = path.extname(file.name);
-    const name = `${uuidv4()}${filename}`;
-    const folder = req.body.folder;
-    const dir = `./public/uploads/${folder}`;
+  try {
+    if (req.files) {
+      const { file } = req.files;
+      const filename = path.extname(file.name);
+      const name = `${uuidv4()}${filename}`;
+      const folder = req.body.folder;
+      const dir = `./public/uploads/${folder}`;
 
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
 
-    if (!folder)
-      res.status(400).send({
-        message: 'Путь для хранения отсутствует',
-        wasFile: true,
-      });
-
-    file.mv(`${dir}/${name}`, err => {
-      if (err) {
-        res.status(400).send('error occured');
-      } else {
-        res.status(200).send({
-          message: `Файл ${filename} успешно загружен`,
-          url: `${process.env.API}/uploads/${folder}/${name}`,
-          path: `${dir}/${name}`,
-          filename,
+      if (!folder)
+        res.status(400).send({
+          message: 'Путь для хранения отсутствует',
           wasFile: true,
         });
-      }
-    });
-  } else {
+
+      file.mv(`${dir}/${name}`, err => {
+        if (err) {
+          res.status(400).send('error occured');
+        } else {
+          res.status(200).send({
+            message: `Файл ${filename} успешно загружен`,
+            url: `${process.env.API}/uploads/${folder}/${name}`,
+            path: `${dir}/${name}`,
+            filename,
+            wasFile: true,
+          });
+        }
+      });
+    } else {
+      res.status(400).send({
+        message: 'Файл отсутствует',
+        wasFile: false,
+      });
+    }
+  } catch (err) {
+    let text;
+    if (err.code === 'ENOENT') {
+      text = `Сервер: Отсутствует директория \"${err.path}\".`;
+    }
+
     res.status(400).send({
-      message: 'Файл отсутствует',
-      wasFile: false,
+      message: text ?? 'Неизвестная ошибка на сервере',
     });
   }
 };
