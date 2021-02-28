b057955fc8453a8c813db450ee29a8394bf798b5
Веревин <d.verevin@vsemayki.ru>
2021-02-28 14:33:12 +0700

    Refactoring

--- client/src/components/CardProductTabs/CardProductTabs.tsx
+++ client/src/components/CardProductTabs/CardProductTabs.tsx
@@ -1,10 +1,29 @@
 import { selectProductItem } from '@redux/reducer/product';
+import { Product } from '@src/interface';
 import { Button } from '@verevinds/ui-kit';
 import cn from 'classnames';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import styles from './cardproducttabs.module.scss';
+
+function createLine<T extends Product>(
+  value: T[keyof T],
+  name: string,
+  unit?: string,
+) {
+  return (
+    value && (
+      <div className={styles['line']}>
+        <span className={styles['name']}>{name}</span>
+        <span className={styles['dots']} />
+        <span className={styles['value']}>
+          {unit ? `${value} ${unit}` : value}
+        </span>
+      </div>
+    )
+  );
+}
 
 export default function CardProductTabs() {
   const item = useSelector(selectProductItem);
@@ -33,6 +52,7 @@ export default function CardProductTabs() {
     noiseInside,
     energyEfficiency,
   } = item;
+
   return (
     <section className={styles['tabs']}>
       <div className={styles['block']}>
@@ -59,6 +79,7 @@ export default function CardProductTabs() {
           Характеристики
         </Button>
       </div>
+
       {description && isDescription ? (
         <div className={styles['content']}>
           <h2>Описание товара</h2>
@@ -70,112 +91,36 @@ export default function CardProductTabs() {
       ) : (
         <div className={styles['content']}>
           <h2>Основные характеристики</h2>
-          {type && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Тип</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{type}</span>
-            </div>
+          {createLine(type, 'Тип')}
+
+          {createLine(servicedArea, 'Обслуживаемая площадь')}
+
+          {createLine(powerCooling, 'Мощность (охлаждение)', 'кВт')}
+          {createLine(powerHeating, 'Мощность (обогрев)', 'кВт')}
+
+          {createLine(
+            powerConsumptionCooling,
+            'Потребляемая мощность (охлаждение)',
+            'Вт',
           )}
-          {servicedArea && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Обслуживаемая площадь</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{servicedArea}</span>
-            </div>
+          {createLine(
+            powerConsumptionHeating,
+            'Потребляемая мощность (нагрев)',
+            'Вт',
           )}
-          {powerCooling && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Мощность (охлаждение)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${powerCooling} кВт`}</span>
-            </div>
-          )}
-          {powerHeating && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Мощность (обогрев)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${powerHeating} кВт`}</span>
-            </div>
-          )}
-          {powerConsumptionCooling && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>
-                Потребляемая мощность (охлаждение)
-              </span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>
-                {`${powerConsumptionCooling} Вт`}
-              </span>
-            </div>
-          )}
-          {powerConsumptionHeating && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>
-                Потребляемая мощность (нагрев)
-              </span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>
-                {`${powerConsumptionHeating} Вт`}
-              </span>
-            </div>
-          )}
-          {energyEfficiency && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Класс энергоэффективности</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{energyEfficiency}</span>
-            </div>
-          )}
-          {noiseInside && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Уровень шума (внутри)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${noiseInside} дБ`}</span>
-            </div>
-          )}
-          {noiseOutside && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Уровень шума (снаружи)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${noiseOutside} дБ`}</span>
-            </div>
-          )}
-          {sizeIndoor && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Размер (Внутренний блок)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${sizeIndoor} мм`}</span>
-            </div>
-          )}
-          {sizeOutdoor && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Размер (Наружный блок)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${sizeOutdoor} мм`}</span>
-            </div>
-          )}
-          {weightIndoor && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Вес (Внутренний блок)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${weightIndoor} кг`}</span>
-            </div>
-          )}
-          {weightOutdoor && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Вес (Наружный блок)</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${weightOutdoor} кг`}</span>
-            </div>
-          )}
-          {warranty && (
-            <div className={styles['line']}>
-              <span className={styles['name']}>Гарантия</span>
-              <span className={styles['dots']} />
-              <span className={styles['value']}>{`${warranty} мес`}</span>
-            </div>
-          )}
+
+          {createLine(energyEfficiency, 'Класс энергоэффективности')}
+
+          {createLine(noiseInside, 'Уровень шума (внутри)', 'дБ')}
+          {createLine(noiseOutside, 'Уровень шума (снаружи)', 'дБ')}
+
+          {createLine(sizeIndoor, 'Размер (Внутренний блок)', 'мм')}
+          {createLine(sizeOutdoor, 'Размер (Наружный блок)', 'мм')}
+
+          {createLine(weightIndoor, 'Вес (Внутренний блок)', 'кг')}
+          {createLine(weightOutdoor, 'Вес (Наружный блок)', 'кг')}
+
+          {createLine(warranty, 'Гарантия', 'мес')}
         </div>
       )}
     </section>
