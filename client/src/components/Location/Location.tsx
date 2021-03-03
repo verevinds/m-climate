987c1479ea5438b736914a2b626bc815257b14f3
Веревин Денис <d.verevin@vsemayki.ru>
2021-03-03 11:30:59 +0700

    Add subdomain

--- client/src/components/Location/Location.tsx
+++ client/src/components/Location/Location.tsx
@@ -1,29 +1,16 @@
 import MapMarkerAlt from '@public/svg/map-marker-alt.svg';
-import {
-  selectGeoCity,
-  turnKDA,
-  turnNSK,
-} from '@redux/reducer/application/geo';
-import { Button } from '@verevinds/ui-kit';
+import { selectGeoCity } from '@redux/reducer/application/geo';
 import cn from 'classnames';
 import { useCallback, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import styles from './location.module.scss';
 
 const Location = () => {
-  const dispatch = useDispatch();
   const [show, toggleShow] = useState(false);
   const handleShow = useCallback(() => toggleShow(!show), [show]);
   const city = useSelector(selectGeoCity);
-  const toggleNSK = () => {
-    dispatch(turnNSK());
-    handleShow();
-  };
-  const toggleKDA = () => {
-    dispatch(turnKDA());
-    handleShow();
-  };
+
   return (
     <div className={styles.location}>
       <button className={styles.button} type='button' onClick={handleShow}>
@@ -36,12 +23,18 @@ const Location = () => {
       </button>
       <div className={cn(styles['dropdown'], show && styles['dropdown-show'])}>
         <h2 className={styles['dropdown__title']}>Выберите город</h2>
-        <Button className={styles['dropdown__element']} onClick={toggleNSK}>
+        <a
+          className={styles['dropdown__element']}
+          href={`http://nsk.${process.env.url}`}
+        >
           Новосибирск
-        </Button>
-        <Button className={styles['dropdown__element']} onClick={toggleKDA}>
+        </a>
+        <a
+          className={styles['dropdown__element']}
+          href={`http://kda.${process.env.url}`}
+        >
           Красноярск
-        </Button>
+        </a>
       </div>
     </div>
   );
