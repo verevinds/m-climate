865bbb7b4f0a1379134dd7ef9055bb90e54c8cd2
Веревин Денис <d.verevin@vsemayki.ru>
2021-03-10 07:44:59 +0700

    Add styles for img with link

--- client/src/components/Banners/Banners.tsx
+++ client/src/components/Banners/Banners.tsx
@@ -1,6 +1,8 @@
+import Img from '@components/Img';
 import Slider from '@components/Slider/Slider';
 import { getBanners, selectBannersList } from '@redux/reducer/banners';
 import ActiveLink from '@src/utils/ActiveLink';
+import cn from 'classnames';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -24,32 +26,25 @@ const Banners = () => {
       pagination={{ clickable: true }}
       className={styles['slider']}
     >
-      {banners.map(banner => (
-        <ActiveLink key={banner._id} href={banner.href}>
-          <picture className={styles['full-size']}>
-            <source
-              srcSet={`${banner.url.substr(
-                0,
-                banner.url.lastIndexOf('.'),
-              )}.avif`}
-              type='image/avif'
-            />
-            <source
-              srcSet={`${banner.url.substr(
-                0,
-                banner.url.lastIndexOf('.'),
-              )}.webp`}
-              type='image/webp'
-            />
-            <img
+      {banners.map(banner =>
+        banner.href ? (
+          <ActiveLink key={banner._id} href={banner.href}>
+            <Img
               src={banner.url}
               alt={banner.name}
-              className={styles['full-size']}
+              className={cn(styles['full-size'], styles['cursor'])}
               loading='lazy'
             />
-          </picture>
-        </ActiveLink>
-      ))}
+          </ActiveLink>
+        ) : (
+          <Img
+            src={banner.url}
+            alt={banner.name}
+            className={styles['full-size']}
+            loading='lazy'
+          />
+        ),
+      )}
     </Slider>
   );
 };
